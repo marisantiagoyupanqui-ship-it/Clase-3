2952dafbebfd65f4827b2d9931a07bd965fbe8bb
marisantiagoyupanqui-ship-it <marisantiagoyupanqui@gmail.com>
2026-05-16 17:52:34 -0500

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,29 @@
+public class Variables {
+    public static void main(String[] args) {
+ byte byteDato = 100;
+short shortDato = 1000;
+int intDato = 1000000;
+ long longDato = 1000000000000L;
+ float floatDato = 100.25f;
+ double doubleDato = 2000.35;
+ boolean booleanDato = true;
+char charDato ='\u0040';
+
+ System.out.print("o byte:");
+ System.out.println(byteDato);
+ System.out.print("ato short:");
+ System.out.println(shortDato);
+ System.out.print("Dato int: ");
+System.out.println(intDato);
+ System.out.print("Dato long: ");
+System.out.println(longDato);
+ System.out.print("to float: ");
+System.out.println(floatDato);
+ System.out.print("ato double:");
+ System.out.println(doubleDato);
+ System.out.print("ato boolean:");
+ System.out.println(booleanDato);
+ System.out.print("ato char:");
+System.out.println(charDato);
+} // Fin del método main
+} // Fin de la clase Variables
